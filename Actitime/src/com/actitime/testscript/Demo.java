ca366af1bc2b3352a62e9a21914ccc8c02654e0a
Ashokgongadi <117472959+Ashokgongadi@users.noreply.github.com>
2023-04-11 16:29:28 +0530

    demo

--- Actitime/src/com/actitime/testscript/Demo.java
+++ Actitime/src/com/actitime/testscript/Demo.java
@@ -1,4 +1,6 @@
-public static Demo{
+package com.actitime.testscript;
+
+public class Demo{
   public static void main(String[] args){
     System.out.println("hii");
   }
---
+++ Actitime/src/com/actitime/testscript/Demo2.java
@@ -0,0 +1,9 @@
+package com.actitime.testscript;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
